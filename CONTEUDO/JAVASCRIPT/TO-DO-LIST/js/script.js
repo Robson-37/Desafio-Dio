a28cd36d96b9f7a23cd2464526420f0770079fb1
Robson Luiz <robson.luizsilva2019@gmail.com>
2022-06-22 22:36:19 -0300

    Conclusão do desafio TO-DO LIST

--- CONTEUDO/JAVASCRIPT/TO-DO-LIST/js/script.js
+++ CONTEUDO/JAVASCRIPT/TO-DO-LIST/js/script.js
@@ -1,1 +1,12 @@
-console.log("Ola mundo");
+
+function adicionar(){
+    //Pegando o valor do campo peenchido
+var valorCampo = document.getElementById("txt-afazeres"). value;
+    //criando o elemento
+var novaLista = document.createElement("li");
+var txtConteudo = document.createTextNode(valorCampo);
+
+novaLista.appendChild(txtConteudo);
+
+document.getElementById("material").appendChild(novaLista);
+}
